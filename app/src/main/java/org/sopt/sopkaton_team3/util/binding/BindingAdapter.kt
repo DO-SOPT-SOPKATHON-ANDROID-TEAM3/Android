d931f8fa1ba07a66b98c3c29f5975aae90c22b83
Sangwook123 <operawook@catholic.ac.kr>
2023-11-25 02:11:56 +0900

    [chore] delete useless import

--- app/src/main/java/org/sopt/sopkaton_team3/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/sopt/sopkaton_team3/util/binding/BindingAdapter.kt
@@ -1,13 +1,9 @@
 package org.sopt.sopkaton_team3.util.binding
 
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
-import org.sopt.sopkaton_team3.R
 
 @BindingAdapter("setImageUrl")
 fun ImageView.setImageUrl(imageUrl: String?) {
