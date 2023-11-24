143d8d8d37148c703d53141d5553d499cab35b4e
Sangwook123 <operawook@catholic.ac.kr>
2023-11-25 00:44:18 +0900

    [chore] binding adapter

--- app/src/main/java/org/sopt/sopkaton_team3/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/sopt/sopkaton_team3/util/binding/BindingAdapter.kt
@@ -1,6 +1,9 @@
 package org.sopt.sopkaton_team3.util.binding
 
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
@@ -9,10 +12,9 @@ import org.sopt.sopkaton_team3.R
 @BindingAdapter("setImageUrl")
 fun ImageView.setImageUrl(imageUrl: String?) {
     if (imageUrl == null) {
-        load(R.color.white)
+        return
     } else {
         load(imageUrl) {
-            placeholder(R.color.white)
             transformations(RoundedCornersTransformation(10F))
         }
     }
