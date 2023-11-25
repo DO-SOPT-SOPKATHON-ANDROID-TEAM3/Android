e5faeaabdd20fbe1344daecf2dce9eb84c1fed30
l2zh <dlwnsgml0109@naver.com>
2023-11-26 01:01:05 +0900

    [feat] 프로그레스 뷰 구현

--- app/src/main/java/org/sopt/sopkaton_team3/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/sopt/sopkaton_team3/util/binding/BindingAdapter.kt
@@ -1,9 +1,12 @@
 package org.sopt.sopkaton_team3.util.binding
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
 import coil.load
 import coil.transform.RoundedCornersTransformation
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 @BindingAdapter("setImageUrl")
 fun ImageView.setImageUrl(imageUrl: String?) {
@@ -14,4 +17,14 @@ fun ImageView.setImageUrl(imageUrl: String?) {
             transformations(RoundedCornersTransformation(10F))
         }
     }
+}
+
+@BindingAdapter("left", "max")
+fun TextView.setLeftPeople(left: Long, max: Float) {
+    text = String.format("%d / %.0f", left, max)
+}
+
+@BindingAdapter("app:cpb_progress_max")
+fun setCircularProgressBarMax(circularProgressBar: CircularProgressBar, max: LiveData<Float>) {
+    circularProgressBar.progressMax = max.value?.toFloat() ?: 0f
 }
