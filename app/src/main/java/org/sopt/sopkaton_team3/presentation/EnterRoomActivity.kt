cc9db25c6d89a46ad0f04ac199116e7575b7300c
LEE YOU BIN <youbeene3409@naver.com>
2023-11-26 01:42:10 +0900

    [FEAT] 방 생성 버튼 리스너 구현

--- app/src/main/java/org/sopt/sopkaton_team3/presentation/EnterRoomActivity.kt
+++ app/src/main/java/org/sopt/sopkaton_team3/presentation/EnterRoomActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import org.sopt.sopkaton_team3.R
 
 class EnterRoomActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_room)
--- app/src/main/java/org/sopt/sopkaton_team3/presentation/InitRoomActivity.kt
+++ app/src/main/java/org/sopt/sopkaton_team3/presentation/InitRoomActivity.kt
@@ -1,12 +1,31 @@
 package org.sopt.sopkaton_team3.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.sopkaton_team3.R
+import org.sopt.sopkaton_team3.databinding.ActivityInitRoomBinding
 
 class InitRoomActivity : AppCompatActivity() {
+    lateinit var binding : ActivityInitRoomBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_init_room)
+        setContentView(binding.root)
+
+        initMakeRoomBtnListener()
+        initEnterRoomListener()
+    }
+
+    private fun initMakeRoomBtnListener(){
+        binding.btnMakeRoom.setOnClickListener{
+            val intentMakeRoomActivity = Intent(this, ) // 다음 액티비티 넣기
+            startActivity(intentMakeRoomActivity)
+        }
+    }
+    private fun initEnterRoomListener(){
+        binding.btnEnterRoom.setOnClickListener{
+            val intentEnterRoomActivity = Intent(this, ) // 다음 액티비티 넣기
+            startActivity(intentEnterRoomActivity)
+        }
     }
 }
