23aeb89a444c1abbcffdfc52231513da7bef9815
Sangwook123 <operawook@catholic.ac.kr>
2023-11-26 07:35:50 +0900

    [feat] 야호!

--- app/src/main/java/org/sopt/sopkaton_team3/presentation/result/ResultActivity.kt
+++ app/src/main/java/org/sopt/sopkaton_team3/presentation/result/ResultActivity.kt
@@ -1,11 +1,13 @@
 package org.sopt.sopkaton_team3.presentation.result
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import org.sopt.sopkaton_team3.R
 import org.sopt.sopkaton_team3.databinding.ActivityResultBinding
+import org.sopt.sopkaton_team3.presentation.initRoom.InitRoomActivity
 import org.sopt.sopkaton_team3.util.binding.BindingActivity
 
 class ResultActivity : BindingActivity<ActivityResultBinding>(R.layout.activity_result) {
@@ -17,6 +19,12 @@ class ResultActivity : BindingActivity<ActivityResultBinding>(R.layout.activity_
         val resultAdapter = ResultAdapter()
         name = intent.getStringExtra("name")
         binding.rvResult.adapter = resultAdapter
+
+        binding.ivResultBack.setOnClickListener {
+            val intent = Intent(this, InitRoomActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
 
         resultViewModel.getResultData.observe(this) {
             binding.data = it
