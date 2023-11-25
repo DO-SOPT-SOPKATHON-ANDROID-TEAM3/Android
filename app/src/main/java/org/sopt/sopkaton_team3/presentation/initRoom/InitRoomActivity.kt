d46b347d3a0b9d6bcb042cda21017a4b39477b23
LEE YOU BIN <youbeene3409@naver.com>
2023-11-26 05:37:48 +0900

    [UI] 초기 화면 UI

--- app/src/main/java/org/sopt/sopkaton_team3/presentation/initRoom/InitRoomActivity.kt
+++ app/src/main/java/org/sopt/sopkaton_team3/presentation/initRoom/InitRoomActivity.kt
@@ -1,5 +1,6 @@
 package org.sopt.sopkaton_team3.presentation.initRoom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.sopkaton_team3.databinding.ActivityInitRoomBinding
@@ -17,14 +18,14 @@ class InitRoomActivity : AppCompatActivity() {
 
     private fun initMakeRoomBtnListener(){
         binding.btnMakeRoom.setOnClickListener{
-//            val intentMakeRoomActivity = Intent(this, ) // 다음 액티비티 넣기
-//            startActivity(intentMakeRoomActivity)
+            val intentMakeRoomActivity = Intent(this, )
+            startActivity(intentMakeRoomActivity)
         }
     }
     private fun initEnterRoomListener(){
         binding.btnEnterRoom.setOnClickListener{
-//            val intentEnterRoomActivity = Intent(this, ) // 다음 액티비티 넣기
-//            startActivity(intentEnterRoomActivity)
+            val intentEnterRoomActivity = Intent(this, )
+            startActivity(intentEnterRoomActivity)
         }
     }
 }
