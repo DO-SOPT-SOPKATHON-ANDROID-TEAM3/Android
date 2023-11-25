f288bb8733c2b25ea6dea360229f52d28f06491e
LEE YOU BIN <youbeene3409@naver.com>
2023-11-26 01:28:25 +0900

    [UI] 방 생성, 입장 UI

---
+++ app/src/main/java/org/sopt/sopkaton_team3/presentation/EnterRoomActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.sopkaton_team3.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.sopkaton_team3.R
+
+class EnterRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_room)
+    }
+}
